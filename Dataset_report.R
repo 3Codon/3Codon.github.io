def22a884cd6c59f7017d2a54da3d9921197fc6b
Emiliano Ferro <dr.strange153b@gmail.com>
2025-03-03 22:35:50 -0600

    Actualice el dataset y lo ordene
    Puse en orden cual es la muestra 1 a la 18

--- Dataset_report.R
+++ Dataset_report.R
@@ -1,7 +1,3 @@
 # Convertimos los datos en un dataset 
-data <- read.csv("/Users/emilianoferrorodriguez/Desktop/LCGEJ/gh/3Codon.github.io/content/post/ale_tarea_06/filereport_read_run_PRJNA494527_tsv.csv", header = TRUE, sep = ",")
-
-
-
-
-
+data <- read.csv("/Users/emilianoferrorodriguez/Desktop/LCGEJ/gh/3Codon.github.io/content/post/ale_tarea_06/filereport_read_run_PRJNA494527_tsv-2.csv", header = TRUE, sep = ",")
+datos_ordenados <- data[order(data$sample_title), ]
