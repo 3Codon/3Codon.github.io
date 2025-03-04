daeaf1fee8cd3dea6de0bae3be9da778520304bc
Emiliano Ferro <dr.strange153b@gmail.com>
2025-03-03 22:13:53 -0600

    actualizo

--- Dataset_report.R
+++ Dataset_report.R
@@ -1,5 +1,5 @@
 # Convertimos los datos en un dataset 
-datos <- read.csv("/Users/emilianoferrorodriguez/Desktop/LCGEJ/gh/3Codon.github.io/content/post/ale_tarea_06/filereport_read_run_PRJNA494527_tsv.csv", header = TRUE, sep = ",")
+data <- read.csv("/Users/emilianoferrorodriguez/Desktop/LCGEJ/gh/3Codon.github.io/content/post/ale_tarea_06/filereport_read_run_PRJNA494527_tsv.csv", header = TRUE, sep = ",")
 
 
 
